35c7eebbd4e60946958bb902c9383ccac741c652
Sai-Mohitha <ch.mohitha1999@gmail.com>
2024-04-02 13:46:01 +0000

    Gradient-Generator

---
+++ src/components/GradientDirectionItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+import {List, Button} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {directions, onChangeDirection, isActive} = props
+  const {displayText, value, directionId} = directions
+
+  const onClickButton = () => {
+    onChangeDirection(value, directionId)
+  }
+
+  return (
+    <List>
+      <Button isActive={isActive} onClick={onClickButton} type="button">
+        {displayText}
+      </Button>
+    </List>
+  )
+}
+
+export default GradientDirectionItem
